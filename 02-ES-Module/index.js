f27de3f2498c1f2c227f0ac0af2fffda3b6f37d8
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-03 00:01:15 +0330

    ES Module

---
+++ 02-ES-Module/index.js
@@ -0,0 +1,6 @@
+import mainContent, { dec, inc } from "./script1.js";
+
+mainContent()
+
+console.log(inc(10, 20))
+console.log(dec(20, 10))
---
+++ 02-ES-Module/script1.js
@@ -0,0 +1,7 @@
+export default function mainContent() {
+    console.log('main content')
+}
+
+export const inc = (a, b) => a + b
+
+export const dec = (a, b) => a - b
