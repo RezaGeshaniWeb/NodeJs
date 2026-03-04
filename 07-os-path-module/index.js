deba9f735e1b459f7c0f35f09191e6c50018a459
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-04 11:35:01 +0330

    path module

--- 07-os-path-module/index.js
+++ 07-os-path-module/index.js
@@ -1,1 +1,28 @@
-// path
+const path = require('path')
+
+console.log('mongodb' + path.sep + 'data' + path.sep + 'users')
+
+console.log('mongodb' + path.delimiter + 'data' + path.delimiter + 'users')
+
+console.log(path.basename('mongodb/data/users/index.html'))
+
+console.log(path.basename('mongodb/data/users/index.html', '.html'))
+
+console.log(path.dirname('/mongodb/data/users/index.html'))
+
+console.log(path.extname('/mongodb/data/users/index.html'))
+
+console.log(path.join('/', 'chapter2', 'lesson4', 'model', 'product.model.js'))
+
+console.log(path.isAbsolute('/', 'chapter2', 'lesson4', 'model', 'product.model.js'))
+
+console.log(path.parse('/chapter2/lesson4/model/product.model.js'))
+
+console.log(path.normalize('c://chapter2/lesson4/model//product.model.js'))
+
+console.log(__dirname)
+
+console.log(__filename)
+
+
+
